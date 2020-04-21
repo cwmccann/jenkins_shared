7914f5f52f019086c7ef30952f4fd62a97e84c00
Chris McCann <cwmccann@gmail.com>
2020-04-21 15:42:35 -0700

    WIP

---
+++ src/com/mcb/jenkins/Functions.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * Gets all the branches for a project
+ */
+def getBranches(repository) {
+    def gettags = ("git ls-remote -t -h ${repository}").execute()
+    return gettags.text.readLines().collect { 
+        it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\\^\\{\\}", '')
+    }
+}
