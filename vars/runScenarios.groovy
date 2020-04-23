f37b158bb078b09aaa0073fc9d9028235ff00e16
Chris McCann <cwmccann@gmail.com>
2020-04-23 15:49:15 -0700

    WIP

--- vars/runScenarios.groovy
+++ vars/runScenarios.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String scenarios, String server, String devices) {
+def call(String scenarios,
+         String server,
+         String devices,
+         String mcbtestcasesJobName = '',
+         String mcbtestcasesJobNumber = '') {
   println "Running scenarios [${scenarios}] on $server with devices [${devices}]"
-  
-  println "${env.JOB_NAME}"
-  println "${env}"
 
-  sh "./gradlew clean scenarioRegression -DbuildName=${mcellblockBuildJobName} -DbuildNumber=${mcellblockBuildJobNumber} \
+  sh "./gradlew clean scenarioRegression -DbuildName=${env.JOB_NAME} -DbuildNumber=${env.BUILD_ID} \
         -DmcbtestcasesBuildName=${mcbtestcasesJobName} -DmcbtestcasesBuildNumber=${mcbtestcasesJobNumber} \
         -DbuildUrl=${BUILD_URL} -DscenarioTestName=${scenarios} -Ddevices=${devices} -DserverUri=${server}"
 }
