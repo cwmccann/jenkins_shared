24558c090937a4e6bd5d5567d296c46ca49e958f
Chris McCann <cwmccann@gmail.com>
2020-04-23 05:32:04 -0700

    WIP

--- src/com/mcb/jenkins/Utilities.groovy
+++ src/com/mcb/jenkins/Utilities.groovy
@@ -16,7 +16,8 @@ class Utilities implements Serializable {
 
     //Run curl in silent mode but grab the headers back so
     //we can check them with grep for failures.
-    steps.sh """curl \
+    def output = steps.sh (returnStdout: true,
+      script: """curl \
               --silent \
               --show-error \
               --include \
@@ -24,7 +25,11 @@ class Utilities implements Serializable {
               --url http://${server}/api/testModules/upsert \
               --header 'authorization: Basic c3lzdGVtYWRtaW46TjhNYXJsNWM=' \
               --header 'content-type: multipart/form-data;' \
-              --form 'file=@${testModuleFile}; type=application/java-archive' | grep "HTTP/1.1 500"
-      """
+              --form 'file=@${testModuleFile}; type=application/java-archive'"
+        """)
+    if (!output.contains("HTTP/1.1 500")) {
+      steps.error("Uploading testmodul ${testModuleFile} to ${server} failed")
+    }
+    
   }
 }
