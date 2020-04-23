a884d2783d38997948d3006fcd3a99eaf7411260
Chris McCann <cwmccann@gmail.com>
2020-04-23 05:25:44 -0700

    WIP

--- src/com/mcb/jenkins/Utilities.groovy
+++ src/com/mcb/jenkins/Utilities.groovy
@@ -14,11 +14,13 @@ class Utilities implements Serializable {
   def uploadTestModule(server, testModuleFile) {
     println "Upload test module [$testModuleFile] to ${server}"
 
+    //Run curl in silent mode but grab the headers back so
+    //we can check them with grep for failures.
     steps.sh """curl \
               --silent \
               --show-error \
               --include \
-              --request PUT \              
+              --request PUT \
               --url http://${server}/api/testModules/upsert \
               --header 'authorization: Basic c3lzdGVtYWRtaW46TjhNYXJsNWM=' \
               --header 'content-type: multipart/form-data;' \
